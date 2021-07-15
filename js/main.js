d7b869279ba82952e0419e6bf16b95edf5edd3df
Antonio <antonio.sanna2706@gmail.com>
2021-07-15 15:27:06 +0200

    Push JS

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var prezzoBigliettoByKm = 0.21;
+
+var casualAge = parseInt(prompt("Inserisci la tua età"));
+
+var distanza = parseInt(prompt("Inserisci il numero dei Km del tuo viaggio"));
+
+if (casualAge < 18) {
+    var smallAgePrezzoBiglietto = prezzoBigliettoByKm * distanza * 0.8;
+    document.getElementById('prezzoBiglietto').innerHTML = 'Il prezzo del bigletto: ' + smallAgePrezzoBiglietto.toFixed(2) +'€';
+} else if (casualAge > 65) {
+    var oldPrezzoBiglietto = prezzoBigliettoByKm * distanza * 0.6;
+    document.getElementById('prezzoBiglietto').innerHTML = 'Il prezzo del bigletto: ' + oldAgePrezzoBiglietto.toFixed(2) +'€';
+} else {
+    var normalPrezzobiglietto = prezzoBigliettoByKm * distanza;
+    document.getElementById('prezzoBiglietto').innerHTML = 'Il prezzo del bigletto: ' + oldAgePrezzoBiglietto.toFixed(2) +'€';
+
+}
